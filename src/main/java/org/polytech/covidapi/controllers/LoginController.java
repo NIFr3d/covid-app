38cb378d779c21c5ca1805edb654d15cf1dc9c01
NIFr3d <fred-dls@orange.fr>
2023-09-29 14:20:33 +0200

    creation projet angular

--- src/main/java/org/polytech/covidapi/controllers/LoginController.java
+++ src/main/java/org/polytech/covidapi/controllers/LoginController.java
@@ -20,7 +20,7 @@ public class LoginController {
     
     @PostMapping(path = "/register")
     public void register(@RequestParam String mail, @RequestParam String nom, @RequestParam String prenom, @RequestParam String telephone, @RequestParam String password, @RequestParam Integer roleId, @RequestParam Integer centreId) {
-        if(roleService.getRoleById(centreId).isEmpty() || centreService.getCentreById(centreId).isEmpty()) {
+        if(roleService.getRoleById(roleId).isEmpty() || centreService.getCentreById(centreId).isEmpty()) {
             throw new IllegalArgumentException("Role or centre not found");
         }
         Utilisateur utilisateur = new Utilisateur(
